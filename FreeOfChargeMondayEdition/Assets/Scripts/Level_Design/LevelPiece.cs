64ec597427601a2458821c916d99b04324724fa8
AinarScorp <Einarhallik@gmail.com>
2022-10-17 18:01:53 +0200

    deleted stuff

--- FreeOfChargeMondayEdition/Assets/Scripts/Level_Design/LevelPiece.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class LevelPiece : MonoBehaviour
-{
-    [SerializeField][Range(0,20)] int _difficultyLevel = 0;
-
-    public int DifficultyLevel => _difficultyLevel;
-}
